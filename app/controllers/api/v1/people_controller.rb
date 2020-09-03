4d7e35844d8988777471e5e745821d71ba0ee51d
William Barela <william.barela.dev@gmail.com>
2020-09-03 00:21:43 -0500

    add create action for person to return json

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::PeopleController < ApplicationController
+  def create
+    person = Person.create(person_params)
+    if person.valid?
+      render json: {:errors => false}, status: :created
+    else
+      render json: {:errors => person.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:email, :password, :first_name, :last_name, :summary)
+  end
 end
