4671c23a7f32ee1672d250697f976a598683ea0f
William Barela <william.barela.dev@gmail.com>
2020-09-04 00:35:51 -0500

    add create action of auth controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::AuthController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def create
+    person = Person.find_by(:email => person_login_params[:email])
+    if person && person.authenticate(person_login_params[:password])
+      token = encode_token({:person_id => person.id})
+      render json: {
+        person: {
+          email: person.email
+        },
+        jwt_token: token
+      }, status: :accepted
+    else
+      render json: {:message => "Invalid email or password"}, status: :unauthorized
+    end
+  end
+
+  private
+  def person_login_params
+    params.require(:person).permit(:email, :password)
+  end
 end
