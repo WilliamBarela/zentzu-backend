2d489f4b484edde3eb481c0fc005d0cfcfc1f4d7
William Barela <william.barela.dev@gmail.com>
2020-09-09 00:08:51 -0500

    implement profile method of People controller

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::PeopleController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
+  def profile
+    person = Person.find_by_id(current_user)
+    render json: {
+      :person_id => person.id,
+      :email => person.email
+    }
+  end
+
   def create
     person = Person.create(person_params)
     if person.valid?
