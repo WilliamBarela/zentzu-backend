16c83a9a377f7861cd2eb89c33ce43abc75a7b8d
William Barela <william.barela.dev@gmail.com>
2020-10-22 19:10:25 -0500

    change json string

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -6,10 +6,10 @@ class Api::V1::TasksController < ApplicationController
     if task.save
       messages = ["Task saved!"]
       # render json: { message: { index_array(messages) } }, status: :accepted
-      render json: { :message => { index_array(messages) } }, status: :accepted
+      render json: { :message => index_array(messages) }, status: :accepted
     else
       # render json: { errors: { index_array(task.errors.full_messages) } }, status: :accepted
-      render json: { :errors => { index_array(task.errors.full_messages) } }, status: :accepted
+      render json: { :errors => index_array(task.errors.full_messages) }, status: :accepted
     end
   end
 
